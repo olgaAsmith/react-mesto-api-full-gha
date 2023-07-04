248f54a165e5b9a9ed616fa680e0eaae34e2d8b4
Olga Smith <ms.olgasmith.jj@mail.ru>
2023-07-04 14:27:13 +0300

    fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,7 +12,7 @@ const cors = (req, res, next) => {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-
+  res.header('Access-Control-Allow-Credentials', true);
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
